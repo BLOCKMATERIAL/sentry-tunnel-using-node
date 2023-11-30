b3096712a76a2ba54a2b95da7f668f71df8d333d
Myroslav Hryschenko <miroslavgrischenko@gmail.com>
2023-11-30 13:08:50 +0200

    express

--- server.js
+++ server.js
@@ -1,56 +1,14 @@
-import Fastify from 'fastify'
-import cors from '@fastify/cors'
-const PORT = process.env.PORT || 3030;
+// Import packages
+const express = require("express");
+const home = require("./routes/home");
 
-const fastify = Fastify({
-    logger: false,
-  })
+// Middlewares
+const app = express();
+app.use(express.json());
 
-fastify.register(cors, {
-  origin: '*',
-  methods: ['GET', 'POST'],
-});
+// Routes
+app.use("/home", home);
 
-// fastify.get('/', async (request, reply) => {
-//   return 'Tunnel For Sentry Nolemon /tunnel';
-// });
-
-fastify.get('/', function (request, reply) {
-    reply.send({ status: 'Tunnel For Sentry Nolemon /tunnel' })
-  })
-
-fastify.post('/tunnel', async (request, reply) => {
-  try {
-    const envelope = request.body;
-
-    const pieces = envelope.split('\n');
-
-    const header = JSON.parse(pieces[0]);
-
-    const { host, pathname, username } = new URL(header.dsn);
-
-    const projectId = pathname.slice(1);
-
-    const url = `https://${host}/api/${projectId}/envelope/?sentry_key=${username}`;
-
-    const options = {
-      headers: {
-        'Content-Type': 'fistifylication/x-sentry-envelope',
-      },
-    };
-
-    const response = await fistify.axios.post(url, envelope, options);
-
-    reply.status(201).send({ message: 'Success', data: response?.data });
-  } catch (e) {
-    const error = e?.response || e?.message;
-    reply.status(400).send({ message: 'Invalid request', error });
-  }
-});
-
-try {
-    await fastify.listen({ port: 9002 })
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
+// connection
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Listening to port ${port}`));
