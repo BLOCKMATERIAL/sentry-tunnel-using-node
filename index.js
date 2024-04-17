651a8767627dc06e920524820a3919c9104478b8
Myroslav Hryhschenko <46076319+BLOCKMATERIAL@users.noreply.github.com>
2024-04-17 15:20:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,24 @@
 const express = require("express");
 const tunnel = require("./routes/tunnel");
 const cors = require('cors');
-
-const bodyParser  = require('body-parser') 
+const bodyParser = require('body-parser');
 
 // Middlewares
 const app = express();
-app.use(bodyParser.text({ type: ['text/*', '*/json'], limit: '100mb' }), cors())
+
+// Apply CORS middleware globally
+app.use(cors({
+  origin: '*', // Set the allowed origin or use '*' for all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'] // Specify allowed headers
+}));
+
+// Apply bodyParser middleware
+app.use(bodyParser.text({ type: ['text/*', '*/json'], limit: '100mb' }));
+
 // Routes
 app.use("/home", tunnel);
 
-
-
-// connection
+// Start server
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
